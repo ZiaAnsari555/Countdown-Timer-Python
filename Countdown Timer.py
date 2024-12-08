b504fb62e4bc336d894107eb72f91b2af9733935
Zia Ansari <zia.dev007@gmail.com>
2024-12-08 16:56:25 +0500

    first commit

---
+++ Countdown Timer.py
@@ -0,0 +1,12 @@
+import time
+
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+countdown_timer(10000)  # Countdown from 10 seconds
